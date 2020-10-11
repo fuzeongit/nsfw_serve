b77c3ce11ab7c3da0973a27783e7f47724d7413c
JunJie Fu <245142637@qq.com>
2020-10-11 18:26:21 +0800

    .

--- model/nsfw_level.py
+++ model/nsfw_level.py
@@ -13,13 +13,13 @@ class NsfwLevel(models.Model):
     创建如下几个表的字段
     """
     id = models.IntegerField(primary_key=True)
-    url = models.CharField('url')
+    url = models.CharField('url', max_length=100)
     drawings = models.FloatField('drawings')
     hentai = models.FloatField('hentai')
     neutral = models.FloatField('neutral')
     porn = models.FloatField('porn')
     sexy = models.FloatField('sexy')
-    classify = models.CharField('classify')
+    classify = models.CharField('classify', max_length=100)
 
     # 指定表名 不指定默认APP名字——类名(app_demo_Student)
     class Meta:
